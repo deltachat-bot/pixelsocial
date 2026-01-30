ac13cee5a3a1104137f88ee16f573e84915f9f1d
adbenitez <asieldbenitez@gmail.com>
2026-01-30 19:45:11 +0100

    update webxdc app

--- pixelsocial/util.py
+++ pixelsocial/util.py
@@ -11,7 +11,7 @@ from sqlalchemy import delete, select
 from .cli import cli
 from .orm import Post, session_scope
 
-APP_VERSION = "0.14.0"
+APP_VERSION = "0.15.0"
 XDC_PATH = str(Path(__file__).parent / "app.xdc")
 
 
